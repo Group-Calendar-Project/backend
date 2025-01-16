ade4c6982ab27c188e33deb7683a10f308bec27e
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2025-01-16 21:40:10 +0900

    feat: Spring Security 1차 설정

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.projectlombok:lombok")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
     // querydsl - mongodb
     implementation("com.querydsl:querydsl-mongodb:5.0.0") {
