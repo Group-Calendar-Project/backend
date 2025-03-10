0084f6b2bcf35f7f45ae3067501fccd4a8cfb0cc
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-10 16:12:10 +0900

    feature: 이벤트 수정 API 구현

---
+++ customer/src/main/kotlin/com/gc/api/customer/adapter/out/persistence/event/UpdateEventRepository.kt
@@ -0,0 +1,34 @@
+package com.gc.api.customer.adapter.out.persistence.event
+
+import com.gc.api.customer.application.port.out.persistence.event.UpdateEventPort
+import com.gc.api.customer.application.service.dto.event.UpdateEventDto
+import com.gc.storage.document.event.EventDocument
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
+import org.springframework.data.mongodb.repository.support.QuerydslRepositorySupport
+import org.springframework.stereotype.Repository
+
+@Repository
+class UpdateEventRepository(
+  val operations: MongoOperations,
+): QuerydslRepositorySupport(operations), UpdateEventPort{
+
+  override fun updateEvent(request: UpdateEventDto) {
+
+    val query = Query(Criteria.where("_id").`is`(request.id))
+
+    val update = Update()
+    request.title?.let { update.set("title", it) }
+    request.date?.let { update.set("date", it) }
+    request.startTime?.let { update.set("startTime", it) }
+    request.endTime?.let { update.set("endTime", it) }
+    request.isAllDay?.let { update.set("isAllDay", it) }
+    request.alarm?.let { update.set("alarm", it.name) }
+    request.labelId?.let { update.set("labelId", it) }
+    request.frequency?.let { update.set("frequency", it.name) }
+
+    operations.updateFirst(query, update, EventDocument::class.java)
+  }
+}
--- customer/src/main/kotlin/com/gc/api/customer/domain/service/event/EventCommandService.kt
+++ customer/src/main/kotlin/com/gc/api/customer/domain/service/event/EventCommandService.kt
@@ -1,7 +1,9 @@
 package com.gc.api.customer.domain.service.event
 
 import com.gc.api.customer.application.port.out.persistence.event.PostEventPort
+import com.gc.api.customer.application.port.out.persistence.event.UpdateEventPort
 import com.gc.api.customer.application.service.dto.event.EventServiceRequest
+import com.gc.api.customer.application.service.dto.event.UpdateEventDto
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -9,9 +11,14 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class EventCommandService(
   private val postEventPort: PostEventPort,
+  private val updateEventPort: UpdateEventPort,
 ) {
 
   fun addEvent(eventServiceRequest: EventServiceRequest): String {
     return postEventPort.saveEvent(eventServiceRequest)
+  }
+
+  fun updateEvent(updateEventDto: UpdateEventDto) {
+    updateEventPort.updateEvent(updateEventDto)
   }
 }
