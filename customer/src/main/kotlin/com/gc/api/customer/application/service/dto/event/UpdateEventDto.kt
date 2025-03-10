02f70aad4a81cd67a4d1c1f1aa6e4999b63bb60e
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-10 16:11:38 +0900

    feature: UpdateEventRequest, DTO 작성

---
+++ customer/src/main/kotlin/com/gc/api/customer/application/service/dto/event/UpdateEventDto.kt
@@ -0,0 +1,18 @@
+package com.gc.api.customer.application.service.dto.event
+
+import com.gc.api.customer.domain.model.EventAlarm
+import com.gc.api.customer.domain.model.event.EventFrequency
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class UpdateEventDto(
+  val id: String,
+  val title: String?,
+  val date: LocalDate?,
+  val startTime: LocalTime?,
+  val endTime: LocalTime?,
+  val isAllDay: Boolean?,
+  val alarm: EventAlarm?,
+  val labelId: String?,
+  val frequency: EventFrequency?,
+)
